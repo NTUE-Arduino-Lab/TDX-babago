ef6a5e549d65688c28c7818dfd313e78aae162ae
Uma <sarapan19980221@gmail.com>
2021-11-29 11:47:46 +0800

    fix: div render wrong position

--- src/router/path.js
+++ src/router/path.js
@@ -1,6 +1,6 @@
 export default {
-  home: '/',
-  certainStop: '/certainStop',
-  nearbyStops: '/nearbyStops',
-  certainRoute: '/certainRoute',
+  home: '/thef2e-take-a-bus/',
+  certainStop: '/thef2e-take-a-bus/certainStop',
+  nearbyStops: '/thef2e-take-a-bus/nearbyStops',
+  certainRoute: '/thef2e-take-a-bus/certainRoute',
 };
