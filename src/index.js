ad2735b3f5084225a06755e50e2db83e46586f6e
uma <sarapan19980221@gmail.com>
2022-06-04 06:22:52 +0800

    feat: PWA setting

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import ReactDOM from 'react-dom';
 import './global.scss';
 import Router from './router';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <React.StrictMode>
     <Router />
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/router/path.js
+++ src/router/path.js
@@ -1,6 +1,6 @@
 export default {
-  home: '/thef2e-take-a-bus/',
-  certainStop: '/thef2e-take-a-bus/certainStop',
-  nearbyStops: '/thef2e-take-a-bus/nearbyStops',
-  certainRoute: '/thef2e-take-a-bus/certainRoute',
+  home: '/',
+  certainStop: '/certainStop',
+  nearbyStops: '/nearbyStops',
+  certainRoute: '/certainRoute',
 };
